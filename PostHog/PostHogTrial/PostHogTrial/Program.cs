458e7ef1825d6f676237a0d029451ef5f13ca84c
Krishnakanth Ravi <krishnakanth.ravi@solitontech.com>
2026-03-30 16:33:29 +0530

    Exploration: tried postHog locally using the api key from the online account and the host from online UI. Have basic APIs which that implements the major critical features of the posthog like feature flags, product analytics, group analytics, error tracking and this is in runable state by executing the app from visual studio

--- PostHog/PostHogTrial/PostHogTrial/Program.cs
+++ PostHog/PostHogTrial/PostHogTrial/Program.cs
@@ -16,13 +16,13 @@ namespace PostHogTrial
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+            //// Configure the HTTP request pipeline.
+            //if (!app.Environment.IsDevelopment())
+            //{
+            //    app.UseExceptionHandler("/Home/Error");
+            //    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            //    app.UseHsts();
+            //}
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
@@ -33,7 +33,7 @@ namespace PostHogTrial
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=Demo}/{action=Index}/{id?}");
 
             app.Run();
         }
